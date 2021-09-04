c3461d72bf3886e3fcae53c586abd41deaaf4a51
okitan <okitakunio@gmail.com>
2021-09-04 21:06:38 +0900

    :bulb: implemented buildParameters

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { Property, PropertySchema, UpdatePropertySchema } from "@notionhq/client/build/src/api-types";
+import { PagesCreateParameters, PagesUpdateParameters } from "@notionhq/client/build/src/api-endpoints";
+import {
+  InputPropertyValue,
+  Property,
+  PropertySchema,
+  UpdatePropertySchema,
+} from "@notionhq/client/build/src/api-types";
 import { sheets_v4 } from "googleapis";
 
 export type Database = {
@@ -8,40 +14,37 @@ export type Database = {
   };
 };
 
-export type Data = Array<{
-  $id: string | null;
-  $icon: string | null;
-  $cover: string | null;
-  title: string;
+const metadata = ["$id", "$icon", "$cover"] as const;
+export type Datum = {
+  $id?: string;
+  $icon?: string;
+  $cover?: string;
+  $title: string;
   [x: string]: Value;
-}>;
+};
+type Value = string | number | boolean | string[] | { start: Date; end?: Date } | Date | undefined;
 
-type Value = string | number | boolean | string[] | Date | null;
-
-export function parseData({ data, schema }: { data: sheets_v4.Schema$ValueRange; schema: Database }): Data {
+export function parseData({ data, schema }: { data: sheets_v4.Schema$ValueRange; schema: Database }): Datum[] {
   if (!data.values) return [];
 
   const properties: {
     // Property comes from Retrieve / PropertySchema from Create / UpdatePropertySchema from Update
     [propertyName: string]: Property | PropertySchema | UpdatePropertySchema | null;
-    title: { rich_text: {} };
-  } = schema.properties ? { ...schema.properties, title: { rich_text: {} } } : { title: { rich_text: {} } };
+    $title: { rich_text: {} };
+  } = schema.properties ? { ...schema.properties, $title: { rich_text: {} } } : { $title: { rich_text: {} } };
 
   const header = data.values[0];
-  // TODO: throw Error when $id / $title are included in keys
   const keyMap: { [x: string]: number } = Object.fromEntries(
-    ["$id", "$icon", "$cover", ...Object.keys(properties)].map((key) => [key, header.findIndex((e) => e === key)])
+    [...metadata, ...Object.keys(properties)].map((key) => [key, header.findIndex((e) => e === key)])
   );
-  if (!keyMap.title) throw new Error("you should at least specify $title");
+  if (keyMap.title < 0) throw new Error("you should at least specify $title");
 
   return data.values.slice(1).map((array) => ({
     // metadata
     ...Object.fromEntries(
-      ["$id", "$icon", "$cover"].map((key) => {
+      metadata.map((key) => {
         const index = keyMap[key];
-        const value = index < 0 ? null : array[index];
-
-        return [key, value];
+        return index >= 0 ? [key, array[index]] : [];
       })
     ),
     // properties including title
@@ -50,9 +53,7 @@ export function parseData({ data, schema }: { data: sheets_v4.Schema$ValueRange;
         if (property === null || property === undefined) return [];
 
         const index = keyMap[key];
-        const value = index < 0 ? null : parseValue(array[index], Object.keys(property)[0]);
-
-        return [key, value];
+        return index >= 0 ? [key, parseValue(array[index], Object.keys(property)[0])] : [];
       })
     ),
   }));
@@ -69,16 +70,123 @@ function parseValue(value: any, type: string): Value {
     case "last_edited_by":
       return value ?? "";
     case "number":
-      return value !== undefined ? parseFloat(value) : null;
+      return value !== undefined ? parseFloat(value) : undefined;
     case "checkbox":
       return !!value;
     case "multi_select":
     case "files":
       return value ? value.split(",") : [];
     case "date":
+      if (!value) return undefined;
+
+      if (value.includes("→")) {
+        const [start, end] = value.split("→");
+
+        return { start: new Date(Date.parse(start)), end: new Date(Date.parse(end)) };
+      } else {
+        return { start: new Date(Date.parse(value)) };
+      }
+
     case "created_time":
     case "last_edited_time":
-      return value ? new Date(Date.parse(value)) : null;
+      return value ? new Date(Date.parse(value)) : undefined;
+    default:
+      throw new Error(`unsupported type ${type}`);
+  }
+}
+
+export function buildPageParameters({
+  data,
+  schema,
+}: {
+  data: Datum & { $databaseParent?: string; $pageParent?: string };
+  schema: Database;
+}): PagesCreateParameters | PagesUpdateParameters {
+  if (!data.$id && !data.$databaseParent && !data.$pageParent)
+    throw new Error("either $id or $databaseParent or $pageParent is needed");
+
+  const title = {
+    type: "title",
+    title: [{ type: "text", text: { content: data.$title } }],
+  };
+
+  const parameter = data.$id
+    ? ({ page_id: data.$id, archived: false, properties: { title } } as PagesUpdateParameters)
+    : data.$databaseParent
+    ? ({ parent: { database_id: data.$databaseParent }, properties: { title } } as PagesCreateParameters)
+    : ({ parent: { page_id: data.$pageParnet }, properties: { title } } as PagesCreateParameters);
+
+  if ("$icon" in data && data.$icon) parameter.icon = { type: "emoji", emoji: data.$icon };
+  if ("$cover" in data && data.$cover) parameter.cover = { type: "external", external: { url: data.$cover } };
+
+  const properties = schema.properties;
+  if (!properties) return parameter;
+
+  Object.entries(data).forEach(([key, value]) => {
+    if (key.startsWith("$")) return;
+    const property = properties[key];
+    if (!property) return;
+
+    parameter.properties[key] = buildProperty(value, Object.keys(property)[0]);
+  });
+  return parameter;
+}
+
+function buildProperty(value: Value, type: string): InputPropertyValue {
+  // const property: InputPropertyValue = {};
+  switch (type) {
+    case "rich_text":
+      if (typeof value !== "string") throw new Error(`value should be string for ${type} but ${typeof value}`);
+      return { type, rich_text: [{ type: "text", text: { content: value } }] };
+    case "select":
+      if (typeof value !== "string") throw new Error(`value should be string for ${type} but ${typeof value}`);
+      return { type, select: { name: value } };
+    case "url":
+      if (typeof value !== "string") throw new Error(`value should be string for ${type} but ${typeof value}`);
+      return { type, url: value };
+    case "email":
+      if (typeof value !== "string") throw new Error(`value should be string for ${type} but ${typeof value}`);
+      return { type, email: value };
+    case "phone_number":
+      if (typeof value !== "string") throw new Error(`value should be string for ${type} but ${typeof value}`);
+      return { type, phone_number: value };
+    case "created_by":
+      if (typeof value !== "string") throw new Error(`value should be string for ${type} but ${typeof value}`);
+      return { type, created_by: { object: "user", id: value } };
+    case "last_edited_by":
+      if (typeof value !== "string") throw new Error(`value should be string for ${type} but ${typeof value}`);
+      return { type, last_edited_by: { object: "user", id: value } };
+    case "number":
+      if (typeof value !== "number") throw new Error(`value should be number for ${type} but ${typeof value}`);
+      return { type, number: value };
+    case "checkbox":
+      if (typeof value !== "boolean") throw new Error(`value should be boolean for ${type} but ${typeof value}`);
+      return { type, checkbox: value };
+    case "multi_select":
+      if (!Array.isArray(value)) throw new Error(`value should be arary for ${type} but ${typeof value}`);
+      return { type, multi_select: value.map((e) => ({ name: e })) };
+    case "files":
+      if (!Array.isArray(value)) throw new Error(`value should be arary for ${type} but ${typeof value}`);
+      // FIXME: name
+      return { type, files: value.map((e) => ({ type: "external", name: e, external: { url: e } })) };
+    case "date":
+      if (typeof value !== "object" || Array.isArray(value))
+        throw new Error(`value should be object for ${type} but ${typeof value}`);
+      if (!("start" in value)) throw new Error(`value shold be { start: Date, end?: Date} for ${type}`);
+
+      return { type, date: { start: value.start.toISOString(), end: value.end?.toISOString() } };
+    case "created_time":
+      if (typeof value !== "object" || Array.isArray(value))
+        throw new Error(`value should be object for ${type} but ${typeof value}`);
+      if ("start" in value) throw new Error(`value shold not be { start: Date, end?: Date} for ${type}`);
+
+      return { type, created_time: value.toISOString() };
+    case "last_edited_time":
+      if (typeof value !== "object" || Array.isArray(value))
+        throw new Error(`value should be object for ${type} but ${typeof value}`);
+      if ("start" in value) throw new Error(`value shold not be { start: Date, end?: Date} for ${type}`);
+
+      return { type, last_edited_time: value.toISOString() };
     default:
       throw new Error(`unsupported type ${type}`);
   }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,6 @@
 import { sheets_v4 } from "googleapis";
 
-import { Database, parseData } from "../src";
+import { buildPageParameters, Database, Datum, parseData } from "../src";
 
 describe(parseData, () => {
   test("works", () => {
@@ -9,13 +9,14 @@ describe(parseData, () => {
         // header
         [
           "$id",
-          "title",
-          "$icon",
+          "$title",
+          "$cover",
           "Text",
           "Number",
           "Select",
           "MultiSelect",
           "Date",
+          "DateWithEnd",
           "Files",
           "Checkbox",
           "Url",
@@ -36,12 +37,13 @@ describe(parseData, () => {
         [
           "deadbeefdeadbeefdeadbeefdeadbeef",
           "A title",
-          "https://example.com/icon.png",
+          "https://example.com/cover.png",
           "A text",
           0,
           "A select",
           "SelectA,SelectB",
           "2021/09/04",
+          "2015/01/10 → 2015/01/16",
           "http://example.com/,https://example.com/",
           "TRUE",
           "https://example.com/",
@@ -68,6 +70,7 @@ describe(parseData, () => {
         Select: { select: {} },
         MultiSelect: { multi_select: {} },
         Date: { date: {} },
+        DateWithEnd: { date: {} },
         Files: { files: {} },
         Checkbox: { checkbox: {} },
         Url: { url: {} },
@@ -86,31 +89,223 @@ describe(parseData, () => {
       },
     };
 
-    expect(parseData({ data, schema })).toMatchObject([
-      {
-        $id: "deadbeefdeadbeefdeadbeefdeadbeef",
-        title: "A title",
-        $icon: "https://example.com/icon.png",
-        Text: "A text",
-        Number: 0,
-        Select: "A select",
-        MultiSelect: ["SelectA", "SelectB"],
-        Date: new Date(2021, 8, 4),
-        Files: ["http://example.com/", "https://example.com/"],
-        Checkbox: true,
-        Url: "https://example.com/",
-        Email: "no-reply@exmple.com",
-        PhoneNumber: "06-6012-3456",
-        CreatedTime: new Date(2021, 8, 4, 15, 35),
-        CreatedBy: "okitan",
-        LastEditedTime: new Date(2021, 8, 4, 15, 40),
-        LastEditedBy: "okita",
-        TextUndefined: "",
-        NumberUndefined: null,
-        CheckboxUndefined: false,
-        MultiSelectUndefined: [],
-        DateUndefined: null,
+    expect(parseData({ data, schema })).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "$cover": "https://example.com/cover.png",
+    "$id": "deadbeefdeadbeefdeadbeefdeadbeef",
+    "$title": "A title",
+    "Checkbox": true,
+    "CheckboxUndefined": false,
+    "CreatedBy": "okitan",
+    "CreatedTime": 2021-09-04T06:35:00.000Z,
+    "Date": Object {
+      "start": 2021-09-03T15:00:00.000Z,
+    },
+    "DateUndefined": undefined,
+    "DateWithEnd": Object {
+      "end": 2015-01-15T15:00:00.000Z,
+      "start": 2015-01-09T15:00:00.000Z,
+    },
+    "Email": "no-reply@exmple.com",
+    "Files": Array [
+      "http://example.com/",
+      "https://example.com/",
+    ],
+    "LastEditedBy": "okita",
+    "LastEditedTime": 2021-09-04T06:40:00.000Z,
+    "MultiSelect": Array [
+      "SelectA",
+      "SelectB",
+    ],
+    "MultiSelectUndefined": Array [],
+    "Number": 0,
+    "NumberUndefined": undefined,
+    "PhoneNumber": "06-6012-3456",
+    "Select": "A select",
+    "Text": "A text",
+    "TextUndefined": "",
+    "Url": "https://example.com/",
+    "undefined": undefined,
+  },
+]
+`);
+  });
+});
+
+describe(buildPageParameters, () => {
+  test("works", () => {
+    const schema: Database = {
+      properties: {
+        Text: { rich_text: {} },
+        Number: { number: {} },
+        Select: { select: {} },
+        MultiSelect: { multi_select: {} },
+        Date: { date: {} },
+        Files: { files: {} },
+        Checkbox: { checkbox: {} },
+        Url: { url: {} },
+        Email: { email: {} },
+        PhoneNumber: { phone_number: {} },
+        CreatedTime: { created_time: {} },
+        CreatedBy: { created_by: {} },
+        LastEditedTime: { last_edited_time: {} },
+        LastEditedBy: { last_edited_by: {} },
+        NotInData: { rich_text: {} },
       },
-    ]);
+    };
+
+    const data: Datum = {
+      $id: "deadbeefdeadbeefdeadbeefdeadbeef",
+      $icon: "😀",
+      $cover: "https://example.com/icon.png",
+      $title: "A title",
+      Text: "A text",
+      Number: 0,
+      Select: "A select",
+      MultiSelect: ["SelectA", "SelectB"],
+      Date: { start: new Date(2021, 8, 4) },
+      DateWithEnd: { start: new Date(2021, 8, 4), end: new Date(2021, 8, 5) },
+      Files: ["http://example.com/", "https://example.com/"],
+      Checkbox: true,
+      Url: "https://example.com/",
+      Email: "no-reply@exmple.com",
+      PhoneNumber: "06-6012-3456",
+      CreatedTime: new Date(2021, 8, 4, 15, 35),
+      CreatedBy: "okitan",
+      LastEditedTime: new Date(2021, 8, 4, 15, 40),
+      LastEditedBy: "okita",
+      TextUndefined: "",
+      NumberUndefined: null,
+      CheckboxUndefined: false,
+      MultiSelectUndefined: [],
+      DateUndefined: null,
+    };
+
+    expect(buildPageParameters({ data, schema })).toMatchInlineSnapshot(`
+Object {
+  "archived": false,
+  "cover": Object {
+    "external": Object {
+      "url": "https://example.com/icon.png",
+    },
+    "type": "external",
+  },
+  "icon": Object {
+    "emoji": "😀",
+    "type": "emoji",
+  },
+  "page_id": "deadbeefdeadbeefdeadbeefdeadbeef",
+  "properties": Object {
+    "Checkbox": Object {
+      "checkbox": true,
+      "type": "checkbox",
+    },
+    "CreatedBy": Object {
+      "created_by": Object {
+        "id": "okitan",
+        "object": "user",
+      },
+      "type": "created_by",
+    },
+    "CreatedTime": Object {
+      "created_time": "2021-09-04T06:35:00.000Z",
+      "type": "created_time",
+    },
+    "Date": Object {
+      "date": Object {
+        "end": undefined,
+        "start": "2021-09-03T15:00:00.000Z",
+      },
+      "type": "date",
+    },
+    "Email": Object {
+      "email": "no-reply@exmple.com",
+      "type": "email",
+    },
+    "Files": Object {
+      "files": Array [
+        Object {
+          "external": Object {
+            "url": "http://example.com/",
+          },
+          "name": "http://example.com/",
+          "type": "external",
+        },
+        Object {
+          "external": Object {
+            "url": "https://example.com/",
+          },
+          "name": "https://example.com/",
+          "type": "external",
+        },
+      ],
+      "type": "files",
+    },
+    "LastEditedBy": Object {
+      "last_edited_by": Object {
+        "id": "okita",
+        "object": "user",
+      },
+      "type": "last_edited_by",
+    },
+    "LastEditedTime": Object {
+      "last_edited_time": "2021-09-04T06:40:00.000Z",
+      "type": "last_edited_time",
+    },
+    "MultiSelect": Object {
+      "multi_select": Array [
+        Object {
+          "name": "SelectA",
+        },
+        Object {
+          "name": "SelectB",
+        },
+      ],
+      "type": "multi_select",
+    },
+    "Number": Object {
+      "number": 0,
+      "type": "number",
+    },
+    "PhoneNumber": Object {
+      "phone_number": "06-6012-3456",
+      "type": "phone_number",
+    },
+    "Select": Object {
+      "select": Object {
+        "name": "A select",
+      },
+      "type": "select",
+    },
+    "Text": Object {
+      "rich_text": Array [
+        Object {
+          "text": Object {
+            "content": "A text",
+          },
+          "type": "text",
+        },
+      ],
+      "type": "rich_text",
+    },
+    "Url": Object {
+      "type": "url",
+      "url": "https://example.com/",
+    },
+    "title": Object {
+      "title": Array [
+        Object {
+          "text": Object {
+            "content": "A title",
+          },
+          "type": "text",
+        },
+      ],
+      "type": "title",
+    },
+  },
+}
+`);
   });
 });
