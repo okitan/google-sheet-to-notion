43ea8322c5b06f625a9dd7506a259763a214d915
okitan <okitakunio@gmail.com>
2021-09-04 16:55:02 +0900

    :bulb: add loadData (parse google sheet values into notion page value)

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import { Property, PropertySchema } from "@notionhq/client/build/src/api-types";
+import { sheets_v4 } from "googleapis";
+
+export type Database = {
+  [x: string]: unknown;
+  properties: {
+    [propertyName: string]: Property | PropertySchema;
+  };
+};
+
+// "rich_text", "number", "select", "multi_select", "date", "formula", "relation", "rollup", "title", "people", "files", "checkbox", "url", "email", "phone_number", "created_time", "created_by", "last_edited_time", and "last_edited_by".
+
+export function loadData({ data, schema }: { data: sheets_v4.Schema$ValueRange; schema: Database }) {
+  if (!data.values) return [];
+
+  const header = data.values[0];
+  const keyMap: { [x: string]: number | undefined } = Object.fromEntries(
+    Object.keys(schema.properties).map((key) => [key, header.findIndex((e) => e === key)])
+  );
+
+  //
+  return data.values.slice(1).map((array) => ({
+    $id: array[0],
+    $title: array[1],
+    ...Object.fromEntries(
+      Object.entries(schema.properties).map(([key, property]) => {
+        const index = keyMap[key];
+        const value = typeof index === "undefined" ? null : parseValue(array[index], Object.keys(property)[0]);
+
+        return [key, value];
+      })
+    ),
+  }));
+
+  // const properties = Object.fromEntries(Object.entries(schema.properties).map(([a, b]) => [a, b]));
+}
+
+function parseValue(value: any, type: string): string | number | boolean | string[] | Date {
+  switch (type) {
+    case "rich_text":
+    case "select":
+    case "url":
+    case "email":
+    case "phone_number":
+    case "created_by":
+    case "last_edited_by":
+      return value;
+    case "number":
+      return parseFloat(value);
+    case "checkbox":
+      return !!value;
+    case "multi_select":
+    case "files":
+      return value.split(",");
+    case "date":
+    case "created_time":
+    case "last_edited_time":
+      return new Date(Date.parse(value));
+    default:
+      throw new Error(`unsupported type ${type}`);
+  }
+}
---
+++ tests/index.test.ts
@@ -0,0 +1,93 @@
+import { sheets_v4 } from "googleapis";
+
+import { Database, loadData } from "../src";
+
+describe(loadData, () => {
+  test("works", () => {
+    const data: sheets_v4.Schema$ValueRange = {
+      values: [
+        // header
+        [
+          "$id",
+          "TitleName",
+          "Text",
+          "Number",
+          "Select",
+          "MultiSelect",
+          "Date",
+          "Files",
+          "Checkbox",
+          "Url",
+          "Email",
+          "PhoneNumber",
+          "CreatedTime",
+          "CreatedBy",
+          "LastEditedTime",
+          "LastEditedBy",
+          "NotInSchema",
+        ],
+        // data
+        [
+          "deadbeefdeadbeefdeadbeefdeadbeef",
+          "A title",
+          "A text",
+          1,
+          "A select",
+          "SelectA,SelectB",
+          "2021/09/04",
+          "http://example.com/,https://example.com/",
+          "TRUE",
+          "https://example.com/",
+          "no-reply@exmple.com",
+          "06-6012-3456",
+          "2021/09/04 15:35",
+          "okitan",
+          "2021/09/04 15:40",
+          "okita",
+          "Never Seen",
+        ],
+      ],
+    };
+
+    const schema: Database = {
+      properties: {
+        Text: { rich_text: {} },
+        Number: { number: {} },
+        Select: { select: {} },
+        MultiSelect: { multi_select: {} },
+        Date: { date: {} },
+        Files: { files: {} },
+        Checkbox: { checkbox: {} },
+        Url: { url: {} },
+        Email: { email: {} },
+        PhoneNumber: { phone_number: {} },
+        CreatedTime: { created_time: {} },
+        CreatedBy: { created_by: {} },
+        LastEditedTime: { last_edited_time: {} },
+        LastEditedBy: { last_edited_by: {} },
+        NotInData: { rich_text: {} },
+      },
+    };
+
+    expect(loadData({ data, schema })).toMatchObject([
+      {
+        $id: "deadbeefdeadbeefdeadbeefdeadbeef",
+        $title: "A title",
+        Text: "A text",
+        Number: 1,
+        Select: "A select",
+        MultiSelect: ["SelectA", "SelectB"],
+        Date: new Date(2021, 8, 4),
+        Files: ["http://example.com/", "https://example.com/"],
+        Checkbox: true,
+        Url: "https://example.com/",
+        Email: "no-reply@exmple.com",
+        PhoneNumber: "06-6012-3456",
+        CreatedTime: new Date(2021, 8, 4, 15, 35),
+        CreatedBy: "okitan",
+        LastEditedTime: new Date(2021, 8, 4, 15, 40),
+        LastEditedBy: "okita",
+      },
+    ]);
+  });
+});
